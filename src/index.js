c6934532944f931059391177d8c88f2e3e26ab85
FlaviaFavilla <favilla.flavia@gmail.com>
2018-05-01 19:30:04 +0200

    twig

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 process.env.NODE_ENV === 'development' && require('./index.html')
 import './scss/main.scss';
 
+var template = require("./prova.html.twig");
+var html = template({title: 'dialog title'});
+
 console.log('ok')
--- webpack.config.js
+++ webpack.config.js
@@ -19,18 +19,18 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader',]
-            }, 
+            },
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader']               
+                use: ['style-loader', 'css-loader', 'sass-loader']
             },
             {
                 test: /\.html$/,
                 use: ["raw-loader"]
             },
-            { 
-                test: /\.twig$/, 
-                use: "twig-loader" 
+            {
+                test: /\.twig$/,
+                use: ["twig-loader"]
             }
         ],
     },
