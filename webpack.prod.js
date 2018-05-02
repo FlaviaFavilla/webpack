b5b0284eb6b47628d13fe115528646874a9efbc8
FlaviaFavilla <favilla.flavia@gmail.com>
2018-05-02 16:22:51 +0200

    versione stabile

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,43 +5,27 @@ const common = require('./webpack.common.js');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');  // crea html da punto di inizio
 const CleanWebpackPlugin = require('clean-webpack-plugin'); //cancella la cart Dist
-//const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production'
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); //estrae il file css
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin"); // minimizza il css
 
 module.exports = merge(common, {
   optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.css$/,
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   },
     module: {
         rules: [
-          {
-            test: /\.s?[ac]ss$/,
-            use: [
-              devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-              'css-loader',
-              //'postcss-loader',
-              'sass-loader',
-            ],
-          },
             {
               test: /\.scss$/,
-              //  use: ['style-loader', 'css-loader',]
               use: [ MiniCssExtractPlugin.loader, "css-loader", 'sass-loader']
             },
-            // {
-            //     test: /\.scss$/,
-            //     use: ['css-loader', 'sass-loader']
-            // },
             {
                 test: /\.html$/,
                 use: ["raw-loader"]  //allows importing files as a String
@@ -58,24 +42,15 @@ module.exports = merge(common, {
         new HtmlWebpackPlugin({
             template:  './src/index.html' // da dove prende index html
         }),
-        // new UglifyJSPlugin({  // minifica il codice
-        //   sourceMap: true
-        // }),
         new webpack.DefinePlugin({
           'process.env.NODE_ENV': JSON.stringify('production') //indica ai plugin il mode per ottimizzare i pacchetti
         }),
         new MiniCssExtractPlugin({
-           filename: "[name].css",
-          // chunkFilename: "[id].css"
-          //filename: devMode ? '[name].css' : '[name].[hash].css',
-          //chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
+           filename: "[name].css", //estrae il file css
         })
-        //new webpack.NamedModulesPlugin(), // -dev mode - path relativo del modulo
-        //new webpack.HotModuleReplacementPlugin() //-dev mode - live autocomplete
     ],
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         hot: true
     },
-    //watch: true
 });
